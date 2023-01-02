b1fceb8f57d992969eb536c49db90ff6fee6cf14
Ifeanyichukwu Favour <ifeanyifavour74@gmail.com>
2023-01-02 06:49:40 -0800

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -23,11 +23,13 @@ char *_strstr(char *haystack, char *needle)
 		{
 			do {
 				if (needle[index + 1] == '\0')
+				{
 					 return (haystack);
+				}
 				index++;
 			} while (haystack[index] == needle[index]);
 		}
 		haystack++;
 	}
-	return('\0');
+	return ('\0');
 }
