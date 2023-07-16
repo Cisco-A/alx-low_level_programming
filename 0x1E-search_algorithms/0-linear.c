8672d59c0707842313aa5fcd0559fbc043542353
Ifeanyichukwu Favour <ifeanyifavour74@gmail.com>
2023-07-16 01:49:36 +0100

    Converted indentations to tab

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,17 +9,17 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    if (array != NULL)
-    {
-        for (int i = 0; i < size; i++)
-        {
-            printf("Value checked array[%d] = %d\n", i, array[i]);
-            if (array[i] == value)
-            {
-                return (i);
-            }
-        }
-    }
+	if (array != NULL)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			printf("Value checked array[%d] = %d\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return (i);
+			}
+		}
+	}
 
-    return (-1);
+	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,35 +11,35 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    if (array != NULL)
-    {
-        size_t new_size = size;        
+	if (array != NULL)
+	{
+		size_t new_size = size;        
 
-        while (new_size > 1)
-        {
-            int new_array[] = {};
+		while (new_size > 1)
+		{
+			int new_array[] = {};
 
-            printf("Searching in array: ");
+			printf("Searching in array: ");
 
-            for (int j = 0; j < new_size; j++)
-            {
-                new_array[j] = array[j];
-                printf("%d, ", array[j]);
-            }
+			for (int j = 0; j < new_size; j++)
+			{
+				new_array[j] = array[j];
+				printf("%d, ", array[j]);
+			}
 
-            printf("\n");
+			printf("\n");
 
-            for (int i = 0; i < new_size; i++)
-            {
-                if (array[i] == value)
-                {
-                    return (i);
-                }
-            }
+			for (int i = 0; i < new_size; i++)
+			{
+				if (array[i] == value)
+				{
+					return (i);
+				}
+			}
 
-            new_size = new_size / 2;
-        }
-    }
+			new_size = new_size / 2;
+		}
+	}
 
-    return (-1);
+	return (-1);
 }
