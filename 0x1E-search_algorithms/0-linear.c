b0006e062ce900eaea65b0e911d02260b43a7e40
Ifeanyichukwu Favour <ifeanyifavour74@gmail.com>
2023-07-16 01:18:08 +0100

    Linear search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+/**
+ * linear_search - search for value in an array of integers
+ * @array: array of integers to be searched
+ * @size: size of array
+ * @value: value being searched for
+ * Return: array index or -1
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+    if (array != NULL)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            printf("Value checked array[%d] = %d\n", i, array[i]);
+            if (array[i] == value)
+            {
+                return (i);
+            }
+        }
+    }
+
+    return (-1);
+}
